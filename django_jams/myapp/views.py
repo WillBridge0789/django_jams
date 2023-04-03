fc686b5115956231c7e7f2b9bfb2328d622390d8
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-04-03 23:57:05 +0000

    Views: the skeleton

--- django_jams/myapp/views.py
+++ django_jams/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Artist, Album, Songs, Genre, Playlist
 
 # Create your views here.
+#--------------------------------------------------------------------------------#
+# def ArtistView(request):
+
+# def AlbumView(request):
+
+# def Songs(request):
+
+# def GenreView(request):
+
+# def PlaylistView(request):
